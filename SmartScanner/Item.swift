37c2767ee59706eee3c5e920253aa60ec96c04d3
ozgungungor <ozgun.gungor>
2026-02-15 20:30:27 +0300

    Initial Commit

---
+++ SmartScanner/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SmartScanner
+//
+//  Created by özgün güngör on 15.02.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SmartScannerTests/SmartScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartScannerTests.swift
+//  SmartScannerTests
+//
+//  Created by özgün güngör on 15.02.2026.
+//
+
+import Testing
+@testable import SmartScanner
+
+struct SmartScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
